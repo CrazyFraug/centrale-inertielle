e8631ac7cba9d42b9e7069157e1b44b6669f757f
dtnguyen.210@gmail.com <dtnguyen.210@gmail.com@2b97b117-e35c-a726-7ceb-97fd3eb2b4a4>
2014-07-03 14:21:14 +0000

    -- Ajout chgt_repere_rotation, chgt_repere_translation

--- trunk/source/Mobile.cpp
+++ trunk/source/Mobile.cpp
@@ -1,29 +1,18 @@
 #include "Mobile.h"
+#include <math.h>
 
 using namespace std;
 
 	Mobile::Mobile() {
-		orientation.phi = 0;
-		orientation.teta = 0;
-		orientation.psi = 0;
-
 		position.x = 0;
 		position.y = 0;
 		position.z = 0;
-
-		v_rot.v_phi=0;
-		v_rot.v_teta=0;
-		v_rot.v_psi=0;
 
 		v_tr.v_x=0;
 		v_tr.v_y=0;
 		v_tr.v_z=0;
 
-		t_orientation = clock();
 		t_position = clock();
-		t_v_rot= clock();
-		t_v_tr = clock();
-//modif
 	}
 
 	void Mobile::maj_position(double x, double y, double z) {
@@ -38,15 +27,15 @@ using namespace std;
 		orientation.teta = teta;
 		orientation.psi = psi;
 	}
-	
-	void Mobile::calcul_vitesse(accel_translation translation) {
-		double dt = (clock() - t_v_tr)/CLOCKS_PER_SEC;
+
+	void Mobile::calcul_vitesse(accel_translation translation, float dt) {
+		//float dt = (clock() - t_position)/CLOCKS_PER_SEC;
 		v_tr.v_x += translation.accel_x*dt;
 		v_tr.v_y += translation.accel_y*dt;
 		v_tr.v_z += translation.accel_z*dt;
 
 	}
-	
+
 
 	void Mobile::afficher_mobile()
 	{
@@ -56,5 +45,59 @@ using namespace std;
 		cout << "phi  = " << setw(4) << right << (int)orientation.phi << endl ;
 		cout << "teta = " << setw(4) << right << (int)orientation.teta << endl ;
 		cout << "psi  = " << orientation.psi << endl ;
+
+	}
+
+
+/*
+ * Fonction calcule le changement du repère absolu avec une rotation
+ * In : teta_pitch, teta_roll, teta_yaw - les angles reçus du gyroscope
+ * Remarque : Erreur d'algorithme -> Refaire des calculs!!!!! - 14h21 03/07/2014
+*/
+void Mobile::chgt_repere_rotation(float teta_pitch, float teta_roll, float teta_yaw)
+{
+    if (teta_pitch!=0){
+        //cout << "Il y a une rotation selon l'axe Ox" << endl;
+        position.y *= cos(teta_pitch);
+        position.z *= cos(teta_pitch);
+    }
+    if (teta_roll!=0){
+        //cout << "Il y a une rotation selon l'axe Oy" << endl;
+        position.x *= cos(teta_roll);
+        position.z *= cos(teta_roll);
+    }
+    if (teta_yaw!=0){
+        //cout << "Il y a une rotation selon l'axe Oz" << endl;
+        position.x *= cos(teta_yaw);
+        position.y *= cos(teta_yaw);
+    }
+}
+
+/*
+ * Fonction calcule le changement du repère absolu avec une translation
+ * In : acc_x, acc_y, acc_z - les accélérations reçus de l'accéléromètre
+ * Remarques : Erreur accéléromètre -> erreur de calcul -> Filtre de Kalman à calculer - 14h22 03/07/2014
+*/
+void Mobile::chgt_repere_translation(float acc_x, float acc_y, float acc_z){
+    float dt = (clock() - t_position);
+    t_position = clock();
+    accel_translation accelerometre;
+    accelerometre.accel_x = acc_x;
+    accelerometre.accel_y = acc_y;
+    accelerometre.accel_z = acc_z;
+    calcul_vitesse(accelerometre,dt);
+    position.x += v_tr.v_x*dt;
+    position.y += v_tr.v_y*dt;
+    position.z += v_tr.v_z*dt;
+}
+
+void Mobile::afficher_position()
+	{
+		//cout.width(4);
+		//cout.fill('0');
+		cout << "Position : " << endl ;
+		cout << "X  = " << setw(4) << right << (int)position.x << endl ;
+		cout << "Y = " << setw(4) << right << (int)position.y<< endl ;
+		cout << "Z  = " << position.z << endl ;
 
 	}
