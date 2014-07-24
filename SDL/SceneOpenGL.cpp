d98cc27948730459f9be56faa374f03706fbf1ec
dtnguyen.210@gmail.com <dtnguyen.210@gmail.com@2b97b117-e35c-a726-7ceb-97fd3eb2b4a4>
2014-07-24 14:57:03 +0000

    -- changement fonction majMesures (vect3D->vect4D) -- fonction filefromSensor marche, test n'est pas validé -- ajout fonction writeEntete pour l'écriture l'entête du fichier

--- SDL/SceneOpenGL.cpp
+++ SDL/SceneOpenGL.cpp
@@ -10,9 +10,9 @@ using namespace std;
 using namespace glm;
 
 
-SceneOpenGL::SceneOpenGL(string titre, int largeur, int hauteur):
-	m_titreFenetre(titre), m_largeurFenetre(largeur), m_hauteurFenetre(hauteur),
-	m_fenetre(0), m_contexteOpenGL(0)
+SceneOpenGL::SceneOpenGL(string titre, int largeur, int hauteur) :
+m_titreFenetre(titre), m_largeurFenetre(largeur), m_hauteurFenetre(hauteur),
+m_fenetre(0), m_contexteOpenGL(0)
 {
 
 }
@@ -21,54 +21,54 @@ SceneOpenGL::~SceneOpenGL()
 {
 	SDL_GL_DeleteContext(m_contexteOpenGL);
 	SDL_DestroyWindow(m_fenetre);
-    SDL_Quit();
+	SDL_Quit();
 }
 
 
 bool SceneOpenGL::InitialiserFenetre() {
-	
+
 	//initialisation SDL
-	if(SDL_Init(SDL_INIT_VIDEO) <0)
+	if (SDL_Init(SDL_INIT_VIDEO) <0)
 	{
 		cout << "erreur lors de l'initialisation de la SDL" << SDL_GetError() << endl;
 		SDL_Quit();
 		return false;
 	}
 
-	
+
 	// Version d'OpenGL
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-	
-    // Double Buffer
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
-    // Création de la fenêtre
+	// Double Buffer
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-    m_fenetre = SDL_CreateWindow("Test SDL 2.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
-									800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	// Création de la fenêtre
 
-    if(m_fenetre == 0)
-    {
-        cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() << endl;
-        SDL_Quit();
+	m_fenetre = SDL_CreateWindow("Test SDL 2.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
-        return false;
-    }
+	if (m_fenetre == 0)
+	{
+		cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() << endl;
+		SDL_Quit();
+
+		return false;
+	}
 
 	// Création du contexte OpenGL
 
-    m_contexteOpenGL = SDL_GL_CreateContext(m_fenetre);
+	m_contexteOpenGL = SDL_GL_CreateContext(m_fenetre);
 
-    if(m_contexteOpenGL == 0)
-    {
-        std::cout << "Erreur a la creation du contexte OpenGL :  " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(m_fenetre);
-        SDL_Quit();
+	if (m_contexteOpenGL == 0)
+	{
+		std::cout << "Erreur a la creation du contexte OpenGL :  " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(m_fenetre);
+		SDL_Quit();
 
-        return false;
-    }
+		return false;
+	}
 
 	return true;
 
@@ -77,78 +77,81 @@ bool SceneOpenGL::InitialiserFenetre() {
 
 bool SceneOpenGL::iniGL() {
 
-        // On initialise GLEW
-        GLenum initialisationGLEW( glewInit() );
+	// On initialise GLEW
+	GLenum initialisationGLEW(glewInit());
 
-        // Si l'initialisation a échoué :
-        if(initialisationGLEW != GLEW_OK)
-        {
-            // On affiche l'erreur grâce à la fonction : glewGetErrorString(GLenum code)
-            std::cout << "Erreur d'initialisation de GLEW : " << glewGetErrorString(initialisationGLEW) << std::endl;
+	// Si l'initialisation a échoué :
+	if (initialisationGLEW != GLEW_OK)
+	{
+		// On affiche l'erreur grâce à la fonction : glewGetErrorString(GLenum code)
+		std::cout << "Erreur d'initialisation de GLEW : " << glewGetErrorString(initialisationGLEW) << std::endl;
 
 
-            // On quitte la SDL
+		// On quitte la SDL
 
-            SDL_GL_DeleteContext(m_contexteOpenGL);
-            SDL_DestroyWindow(m_fenetre);
-            SDL_Quit();
+		SDL_GL_DeleteContext(m_contexteOpenGL);
+		SDL_DestroyWindow(m_fenetre);
+		SDL_Quit();
 
-            return false;
-        }
+		return false;
+	}
 
-   // #endif
+	// #endif
 
 	glEnable(GL_DEPTH_TEST);
 
-    return true;
+	return true;
 
-	}
+}
 
 
 void SceneOpenGL::bouclePrincipale()
 {
 
-    bool terminer(false);
-	unsigned int frameRate (1000 / 200);
-    Uint32 debutBoucle(0), finBoucle(0), tempsEcoule(0);
+	bool terminer(false);
+	unsigned int frameRate(1000 / 200);
+	Uint32 debutBoucle(0), finBoucle(0), tempsEcoule(0);
 
 	std::string port = PORTSERIE;
 	int baudRate = BAUD;
 
 	Instrument gyro("gyr1", port, 115200);
-	Traitement trait_basic(&gyro); 
+	Traitement trait_basic(&gyro);
 	/* l'objet qui servira a récupérer les valeurs de l'arduino puis a faire un moyenne sur plusieurs valeurs pour des résultats plus stables*/
 
-//	Mobile gant;
+	/* Fichier sauvegarder données*/
+	//std::fstream myfile;
+	std::string filename = "gyro.txt" ;
+	//	Mobile gant;
 
-	vect3D angle = {0.0,0.0,0.0};
+	vect3D angle = { 0.0, 0.0, 0.0 };
 
-    // Matrices
-    mat4 projection;
-    mat4 modelview;
+	// Matrices
+	mat4 projection;
+	mat4 modelview;
 
-	COORD pos = {0,1};
+	COORD pos = { 0, 1 };
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	Cube lecube(2.0, "Shaders/couleur3D.vert", "Shaders/couleur3D.frag");
 
-    projection = perspective(1.22, (double) m_largeurFenetre / m_hauteurFenetre, 1.0, 100.0);
-    modelview = mat4(1.0);
-
-    // Boucle principale
-    while(!terminer)
-    {
+	projection = perspective(1.22, (double)m_largeurFenetre / m_hauteurFenetre, 1.0, 100.0);
+	modelview = mat4(1.0);
+	trait_basic.writeEntete(filename);
+	// Boucle principale
+	while (!terminer)
+	{
 		debutBoucle = SDL_GetTicks();
-
+		
 		// Gestion des évènements
-        SDL_PollEvent(&m_evenements);
-        if(m_evenements.window.event == SDL_WINDOWEVENT_CLOSE)
-            terminer = true;
+		SDL_PollEvent(&m_evenements);
+		if (m_evenements.window.event == SDL_WINDOWEVENT_CLOSE)
+			terminer = true;
 
 		SetConsoleCursorPosition(console, pos);
 
-        // Nettoyage de l'écran
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// Nettoyage de l'écran
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Placement de la caméra
 		modelview = lookAt(vec3(4, 0, 0), vec3(0, 0, 0), vec3(0, 1, 0));
@@ -156,6 +159,10 @@ void SceneOpenGL::bouclePrincipale()
 		trait_basic.stockerValeurs();
 		trait_basic.afficherValeurs();
 
+		trait_basic.filefromSensor(filename,&gyro);
+		/*vect4D data;
+		data = trait_basic.readDatafromFile(filename);
+		gyro.majMesures(data);*/
 		if (trait_basic.tabFull() == true)
 		{
 
@@ -168,23 +175,23 @@ void SceneOpenGL::bouclePrincipale()
 			modelview = rotate(modelview, (float)(angle.x), vec3(1, 0, 0));
 			modelview = rotate(modelview, (float)(angle.y), vec3(0, 1, 0));
 			modelview = rotate(modelview, (float)(angle.z), vec3(0, 0, 1));
-				// Rotation du repère
-		
+			// Rotation du repère
+
 		} //end if(tabFull == true)
 
 		else cout << " FALSE " << endl;
 		lecube.afficher(projection, modelview);
 
-        // Actualisation de la fenêtre
-        SDL_GL_SwapWindow(m_fenetre);
+		// Actualisation de la fenêtre
+		SDL_GL_SwapWindow(m_fenetre);
 
 		//framerate
 		finBoucle = SDL_GetTicks();
 		tempsEcoule = finBoucle - debutBoucle;
 
-		if(tempsEcoule < frameRate)
+		if (tempsEcoule < frameRate)
 			SDL_Delay(frameRate - tempsEcoule);
 
 
-    }
+	}
 }
