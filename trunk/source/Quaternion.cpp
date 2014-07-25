58834f6352ed3b42da86faa9af305ac6818ed2f0
gburlats@gmail.com <gburlats@gmail.com@2b97b117-e35c-a726-7ceb-97fd3eb2b4a4>
2014-07-25 09:47:32 +0000

    ajout fonctions (non testées) sur les quaternions: calcul produit hamiltonien calcul de norme calcul la rotation d'un vecteur par un quaternion

--- trunk/source/Quaternion.cpp
+++ trunk/source/Quaternion.cpp
@@ -21,3 +21,45 @@ void afficherQuat(quaternion<double> q)
 	cout << q.R_component_4() << endl;
 
 }
+/** 
+* \brief realise la rotation decrite par le quaternion sur le vecteur v
+* \param q quaternion representatn la rotation
+* \param v vecteur sur lequel on effectue la rotation
+* \return un vecteur 3D qui correspond a v apres rotation
+*/
+vect3D rotateVector(quaternion<double> q, vect3D v)
+{
+	quaternion<double> qb(q.R_component_1(), -q.R_component_2(), - q.R_component_3(), -q.R_component_4());
+	quaternion<double> vquat(0, v.x, v.y, v.z);
+	quaternion<double> result = hamiltonProduct(hamiltonProduct(q,vquat),qb);
+	v.x = result.R_component_2();
+	v.y = result.R_component_3();
+	v.z = result.R_component_4();
+	return v;
+}
+
+/**
+* \brief calcul la norme du quaternion q
+* permet de mettre le quaternion sous forme unitaire
+* \return norme du quaternion
+*/
+double calculateNorm(quaternion<double> q)
+{
+	double norm = sqrt(pow(q.R_component_1(),2) + pow (q.R_component_2(),2) + pow(q.R_component_3(),2) + pow(q.R_component_4(),2)); 
+	return norm;
+}
+
+/**
+* \brief produit hamiltonien de 2 quaternions
+*/
+quaternion<double> hamiltonProduct(quaternion<double> q1, quaternion<double> q2)
+{
+	double a,b,c,d;
+	a = (q1.R_component_1()*q2.R_component_1() - q1.R_component_2()*q2.R_component_2() - q1.R_component_3()*q2.R_component_3() - q1.R_component_4()*q2.R_component_4());
+	b = (q1.R_component_1()*q2.R_component_2() + q1.R_component_2()*q2.R_component_1() + q1.R_component_3()*q2.R_component_4() - q1.R_component_4()*q2.R_component_3());
+	c = (q1.R_component_1()*q2.R_component_3() - q1.R_component_2()*q2.R_component_4() + q1.R_component_3()*q2.R_component_1() + q1.R_component_4()*q2.R_component_2());
+	d = (q1.R_component_1()*q2.R_component_4() + q1.R_component_2()*q2.R_component_3() - q1.R_component_3()*q2.R_component_2() + q1.R_component_4()*q2.R_component_1());
+	quaternion<double> result(a,b,c,d);	
+	
+	return result;
+}
